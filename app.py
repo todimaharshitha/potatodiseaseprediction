91366a96bb516443c03c525b8c420a4471d85a02
Tejeswara Murthy palwadi <tejeswarmurthypalwadi@gmail.com>
2023-05-12 15:07:22 +0530

    added jpeg format for image

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ CLASS_NAMES = ['Early Blight','Late Blight','Healthy']
 
 st.title("POTATO LEAF DISEASE PREDICTION")
 
-uploaded_image = st.file_uploader("Upload Potato Leaf Image",type=["jpg","png"])
+uploaded_image = st.file_uploader("Upload Potato Leaf Image",type=["jpg","png","jpeg"])
 if uploaded_image is not None:
     bytes_data = uploaded_image.getvalue()
     image = np.array(Image.open(BytesIO(bytes_data)).resize((224,224)))
@@ -30,7 +30,7 @@ if uploaded_image is not None:
         confidence = np.max(predictions[0])
         up_image = Image.open(uploaded_image)
         st.image(up_image, width = 300,use_column_width=True)
-        st.success("Plant is in {} condition {} and Prediction Confidence = {}%".format(predicted_class,"\n",round(confidence*100,2)))
+        st.success("Leaf is in {} condition {} and Prediction Confidence = {}%".format(predicted_class,"\n",round(confidence*100,2)))
     except Exception as e:
         st.title("Invalid Image")
 
